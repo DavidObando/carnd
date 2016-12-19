ea6e705ff78a0f26d94058a06a275f390f5b39d4
David Obando <daobando@microsoft.com>
2016-12-19 10:34:56 -0800

    2016-12-19 10:34 PST

--- Term1/Stage1/Material/exitramp.py
+++ Term1/Stage1/Material/exitramp.py
@@ -13,10 +13,20 @@ kernel_size = 5
 blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size), 0)
 
 # Define parameters for Canny and run it
-# NOTE: if you try running this code you might want to change these!
 low_threshold = 50
 high_threshold = 150
 edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+# Next we'll create a masked edges image using cv2.fillPoly()
+mask = np.zeros_like(edges)
+ignore_mask_color = 255
+
+# This time we are defining a four sided polygon to mask
+imshape = image.shape
+#vertices = np.array([[(0,imshape[0]),(0, 0), (imshape[1], 0), (imshape[1],imshape[0])]], dtype=np.int32)
+vertices = np.array([[(0,imshape[0]),(imshape[1]*0.25, imshape[0]*0.45), (imshape[1]*0.75, imshape[0]*0.45), (imshape[1],imshape[0])]], dtype=np.int32)
+cv2.fillPoly(mask, vertices, ignore_mask_color)
+masked_edges = cv2.bitwise_and(edges, mask)
 
 # Define the Hough transform parameters
 # Make a blank the same size as our image to draw on
@@ -24,25 +34,25 @@ edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
 # rho and theta are the distance and angular resolution of our grid in Hough space
 # in Hough space, we have a grid laid out along the (theta, rho) axis
 # rho is specified in units of pixels
-rho = 3
+rho = 5
 # theta is specified in radians
-theta = np.pi/180
+theta = (np.pi/180)*1
 # threshold is number of intersections in hough space needed to make it into the output
-threshold = 6
+threshold = 200
 # min_line_length is the mininum acceptable pixel count of a line in the output
-min_line_length = 10
+min_line_length = 80
 # max_line_gap is the maximum acceptable pixel gap between two points making it to a line
-max_line_gap = 3
+max_line_gap = 5
 line_image = np.copy(image)*0 #creating a blank to draw lines on
 
 # Run Hough on edge detected image
-lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]),
                             min_line_length, max_line_gap)
 
 # Iterate over the output "lines" and draw lines on the blank
 for line in lines:
     for x1,y1,x2,y2 in line:
-        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),3)
+        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
 
 # Create a "color" binary image to combine with line image
 color_edges = np.dstack((edges, edges, edges))
