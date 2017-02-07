7e25f241f99a49dd363358f728f98e9963e7f4da
David Obando <daobando@microsoft.com>
2017-02-06 21:54:56 -0800

    Transfer Learning Lab

---
+++ Term1/Labs/CarND-Transfer-Learning-Lab/runvggcifar.cmd
@@ -0,0 +1,1 @@
+python feature_extraction.py --training_file vgg_cifar10_100_bottleneck_features_train.p --validation_file vgg_cifar10_bottleneck_features_validation.p
---
+++ Term1/Labs/CarND-Transfer-Learning-Lab/runvggtraffic.cmd
@@ -0,0 +1,1 @@
+python feature_extraction.py --training_file vgg_traffic_100_bottleneck_features_train.p --validation_file vgg_traffic_bottleneck_features_validation.p
