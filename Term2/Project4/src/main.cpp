02c99f1a2915a129bf88714978cd2fedb3a75984
David Obando <daobando@microsoft.com>
2017-05-29 16:10:44 -0700

    P4 - some changes

--- Term2/Project4/src/main.cpp
+++ Term2/Project4/src/main.cpp
@@ -33,7 +33,7 @@ int main()
   uWS::Hub h;
 
   PID pid;
-  pid.Init(0.02, 0.0004, 9.0);
+  pid.Init(0.05, 0.00005, 0.05);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
