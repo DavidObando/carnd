d922e98fc33e78f1296b09a0dab251ddd82536d4
David Obando <daobando@microsoft.com>
2017-08-15 23:25:51 -0700

    P1 - walkthrough code with collision avoidance

--- Term3/Project1/src/main.cpp
+++ Term3/Project1/src/main.cpp
@@ -203,8 +203,8 @@ int main() {
   // the velocity we'll aim at, gets updated based on environment
   double ref_vel = 0.0;
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-                     uWS::OpCode opCode) {
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane,&ref_vel]
+                     (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -233,7 +233,7 @@ int main() {
 			// Previous path data given to the Planner
 			auto previous_path_x = j[1]["previous_path_x"];
 			auto previous_path_y = j[1]["previous_path_y"];
-			// Previous path's end s and d values 
+			// Previous path's end s and d values
 			double end_path_s = j[1]["end_path_s"];
 			double end_path_d = j[1]["end_path_d"];
 
@@ -241,6 +241,44 @@ int main() {
 			auto sensor_fusion = j[1]["sensor_fusion"];
 
 			double prev_size = previous_path_x.size();
+
+			if (prev_size > 0)
+			{
+				car_s = end_path_s;
+			}
+
+			bool too_close = false;
+
+			// find reference velocity to use
+			for (int i = 0; i < sensor_fusion.size(); ++i)
+			{
+				// car is in my lane
+				float d = sensor_fusion[i][6];
+				if (d >= (4 * lane) && d <= ((4 * lane) + 4))
+				{
+					double vx = sensor_fusion[i][3];
+					double vy = sensor_fusion[i][4];
+					double check_speed = sqrt((vx * vx) + (vy * vy));
+					double check_car_s = sensor_fusion[i][5];
+
+					check_car_s += ((double)prev_size * 0.02 * check_speed); // if using previous points we can project s value out
+					// check s values greater than mine and s gap
+					if (check_car_s > car_s && (check_car_s - car_s) < 30)
+					{
+						too_close = true;
+						break;
+					}
+				}
+			}
+
+			if (too_close)
+			{
+				ref_vel -= .224;
+			}
+			else if (ref_vel < 49.5)
+			{
+				ref_vel += .224;
+			}
 
 			vector<double> ptsx;
 			vector<double> ptsy;
@@ -280,13 +318,76 @@ int main() {
 				ptsy.push_back(ref_y);
 			}
 
-			json msgJson;
+			// In Frenet, add evenly 30m spaced points ahead of the starting reference
+			vector<double> next_wp0 = getXY(car_s + 30, (2 + (4*lane)), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			vector<double> next_wp1 = getXY(car_s + 60, (2 + (4*lane)), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			vector<double> next_wp2 = getXY(car_s + 90, (2 + (4*lane)), map_waypoints_s, map_waypoints_x, map_waypoints_y);
 
+			ptsx.push_back(next_wp0[0]);
+			ptsx.push_back(next_wp1[0]);
+			ptsx.push_back(next_wp2[0]);
+			ptsy.push_back(next_wp0[1]);
+			ptsy.push_back(next_wp1[1]);
+			ptsy.push_back(next_wp2[1]);
+
+			for (int i = 0; i < ptsx.size(); ++i)
+			{
+				//shift car reference angle to 0 degrees
+				double shift_x = ptsx[i] - ref_x;
+				double shift_y = ptsy[i] - ref_y;
+
+				ptsx[i] = (shift_x * cos(0 - ref_yaw)) - (shift_y * sin(0 - ref_yaw));
+				ptsy[i] = (shift_x * sin(0 - ref_yaw)) + (shift_y * cos(0 - ref_yaw));
+			}
+
+			// create a spline
+			tk::spline s;
+
+			// set (x,y) points to the spline
+			s.set_points(ptsx, ptsy);
+
+			// Define the actual (x,y) points we'll use for the planner
 			vector<double> next_x_vals;
 			vector<double> next_y_vals;
 
+			// Start with all of the previous path points from last time
+			for (int i = 0; i < previous_path_x.size(); ++i)
+			{
+				next_x_vals.push_back(previous_path_x[i]);
+				next_y_vals.push_back(previous_path_y[i]);
+			}
 
-			// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+			// Calculate how to break up spline points so that we travel at our desired reference velocity
+			double target_x = 30.0;
+			double target_y = s(target_x);
+			double target_dist = sqrt((target_x * target_x) + (target_y * target_y));
+
+			double x_add_on = 0.0;
+			double N = target_dist / ((ref_vel / 2.24) * 0.02) ;
+
+			// Fill up the rest of our path planner after filling it with previous points, here we will always output 50 points
+			for (int i = 1; i <= 50 - previous_path_x.size(); ++i)
+			{
+				double x_point = x_add_on + (target_x / N);
+				double y_point = s(x_point);
+
+				x_add_on = x_point;
+
+				double x_ref = x_point;
+				double y_ref = y_point;
+
+				// rotate back to normal plane after rotating from earlier
+				x_point = (x_ref * cos(ref_yaw)) - (y_ref * sin(ref_yaw));
+				y_point = (x_ref * sin(ref_yaw)) + (y_ref * cos(ref_yaw));
+
+				x_point += ref_x;
+				y_point += ref_y;
+
+				next_x_vals.push_back(x_point);
+				next_y_vals.push_back(y_point);
+			}
+
+			json msgJson;
 			msgJson["next_x"] = next_x_vals;
 			msgJson["next_y"] = next_y_vals;
 
